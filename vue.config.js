7ac829e1e19023df1a94daaffed3c1770375c42d
andyfree96 <anthonyfree96@gmail.com>
2023-08-17 08:29:30 +0800

    修正公式错误并部署

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/optic-tools",
+  outputDir: path.resolve(__dirname, "./docs"),
 });
